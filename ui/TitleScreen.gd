5a550d95501ffa6ff914c91dc7dc47472b504726
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-04-11 00:14:20 +0200

    first commit

---
+++ ui/TitleScreen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_NewGame_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
